58b687e2da23dab4f41608f9b7a7480ea6f6ae42
MoonMonn2 <tate@tatesworld.com>
2018-12-05 09:20:34 +0100

    Work on Character Stats Display

--- comic/comic_main.js
+++ comic/comic_main.js
@@ -5,144 +5,182 @@ var ctab = document.getElementsByClassName('ctab')
 var i;
 function onLoad() {
     console.log('onload')
-    for (i = 0; i < ctab.length; i++) {
-        console.log(i)
-        ctab[i].setAttribute("style", "color:#000000;top:180px;height:50px;width:20px;background-color:#333333");
-    }
 }
 
 
 function cShow(arg) {
     switch (arg) {
         case 1:
-            ctab[0].setAttribute("hidden", "false")
+            ctab[0].removeAttribute("hidden")
+            ctab[0].setAttribute("style", "display:table-cell;")
             break
         case 2:
-            ctab[1].setAttribute("hidden", "false")
+            ctab[1].removeAttribute("hidden")
+            ctab[1].setAttribute("style", "display:table-cell;")
             break
         case 3:
-            ctab[2].setAttribute("hidden", "false")
+            ctab[2].removeAttribute("hidden")
+            ctab[2].setAttribute("style", "display:table-cell;")
             break
         case 4:
-            ctab[3].setAttribute("hidden", "false")
+            ctab[3].removeAttribute("hidden")
+            ctab[3].setAttribute("style", "display:table-cell;")
             break
         case 5:
-            ctab[4].setAttribute("hidden", "false")
+            ctab[4].removeAttribute("hidden")
+            ctab[4].setAttribute("style", "display:table-cell;")
             break
         case 6:
-            ctab[5].setAttribute("hidden", "false")
+            ctab[5].removeAttribute("hidden")
+            ctab[5].setAttribute("style", "display:table-cell;")
             break
         case 7:
-            ctab[6].setAttribute("hidden", "false")
+            ctab[6].removeAttribute("hidden")
+            ctab[6].setAttribute("style", "display:table-cell;")
             break
         case 8:
-            ctab[7].setAttribute("hidden", "false")
+            ctab[7].removeAttribute("hidden")
+            ctab[7].setAttribute("style", "display:table-cell;")
             break
         case 9:
-            ctab[8].setAttribute("hidden", "false")
+            ctab[8].removeAttribute("hidden")
+            ctab[8].setAttribute("style", "display:table-cell;")
             break
         case 10:
-            ctab[9].setAttribute("hidden", "false")
+            ctab[9].removeAttribute("hidden")
+            ctab[9].setAttribute("style", "display:table-cell;")
             break
         case 11:
-            ctab[10].setAttribute("hidden", "false")
+            ctab[10].removeAttribute("hidden")
+            ctab[10].setAttribute("style", "display:table-cell;")
             break
         case 12:
-            ctab[11].setAttribute("hidden", "false")
+            ctab[11].removeAttribute("hidden")
+            ctab[11].setAttribute("style", "display:table-cell;")
             break
         case 13:
-            ctab[12].setAttribute("hidden", "false")
+            ctab[12].removeAttribute("hidden")
+            ctab[12].setAttribute("style", "display:table-cell;")
             break
         case 14:
-            ctab[13].setAttribute("hidden", "false")
+            ctab[13].removeAttribute("hidden")
+            ctab[13].setAttribute("style", "display:table-cell;")
             break
         case 15:
-            ctab[14].setAttribute("hidden", "false")
+            ctab[14].removeAttribute("hidden")
+            ctab[14].setAttribute("style", "display:table-cell;")
             break
         case 16:
-            ctab[15].setAttribute("hidden", "false")
+            ctab[15].removeAttribute("hidden")
+            ctab[15].setAttribute("style", "display:table-cell;")
             break
         case 17:
-            ctab[16].setAttribute("hidden", "false")
+            ctab[16].removeAttribute("hidden")
+            ctab[16].setAttribute("style", "display:table-cell;")
             break
         case 18:
-            ctab[17].setAttribute("hidden", "false")
+            ctab[17].removeAttribute("hidden")
+            ctab[17].setAttribute("style", "display:table-cell;")
             break
         case 19:
-            ctab[18].setAttribute("hidden", "false")
+            ctab[18].removeAttribute("hidden")
+            ctab[18].setAttribute("style", "display:table-cell;")
             break
         case 20:
-            ctab[19].setAttribute("hidden", "false")
+            ctab[19].removeAttribute("hidden")
+            ctab[19].setAttribute("style", "display:table-cell;")
             break
         case 21:
-            ctab[20].setAttribute("hidden", "false")
+            ctab[20].removeAttribute("hidden")
+            ctab[20].setAttribute("style", "display:table-cell;")
             break
     }
 }
-function chide() {
+function chide(arg) {
     switch (arg) {
         case 1:
             ctab[0].setAttribute("hidden", "true")
+            ctab[0].removeAttribute("style")
             break
         case 2:
             ctab[1].setAttribute("hidden", "true")
+            ctab[1].removeAttribute("style")
             break
         case 3:
             ctab[2].setAttribute("hidden", "true")
+            ctab[2].removeAttribute("style")
             break
         case 4:
             ctab[3].setAttribute("hidden", "true")
+            ctab[3].removeAttribute("style")
             break
         case 5:
             ctab[4].setAttribute("hidden", "true")
+            ctab[4].removeAttribute("style")
             break
         case 6:
             ctab[5].setAttribute("hidden", "true")
+            ctab[5].removeAttribute("style")
             break
         case 7:
             ctab[6].setAttribute("hidden", "true")
+            ctab[6].removeAttribute("style")
             break
         case 8:
             ctab[7].setAttribute("hidden", "true")
+            ctab[7].removeAttribute("style")
             break
         case 9:
             ctab[8].setAttribute("hidden", "true")
+            ctab[8].removeAttribute("style")
             break
         case 10:
             ctab[9].setAttribute("hidden", "true")
+            ctab[9].removeAttribute("style")
             break
         case 11:
             ctab[10].setAttribute("hidden", "true")
+            ctab[10].removeAttribute("style")
             break
         case 12:
             ctab[11].setAttribute("hidden", "true")
+            ctab[11].removeAttribute("style")
             break
         case 13:
             ctab[12].setAttribute("hidden", "true")
+            ctab[12].removeAttribute("style")
             break
         case 14:
             ctab[13].setAttribute("hidden", "true")
+            ctab[13].removeAttribute("style")
             break
         case 15:
             ctab[14].setAttribute("hidden", "true")
+            ctab[14].removeAttribute("style")
             break
         case 16:
             ctab[15].setAttribute("hidden", "true")
+            ctab[15].removeAttribute("style")
             break
         case 17:
             ctab[16].setAttribute("hidden", "true")
+            ctab[16].removeAttribute("style")
             break
         case 18:
             ctab[17].setAttribute("hidden", "true")
+            ctab[17].removeAttribute("style")
             break
         case 19:
             ctab[18].setAttribute("hidden", "true")
+            ctab[18].removeAttribute("style")
             break
         case 20:
             ctab[19].setAttribute("hidden", "true")
+            ctab[19].removeAttribute("style")
             break
         case 21:
             ctab[20].setAttribute("hidden", "true")
+            ctab[20].removeAttribute("style")
             break
     }
 }
