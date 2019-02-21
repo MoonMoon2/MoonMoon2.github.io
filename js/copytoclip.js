facef575f10ea8e996c9184c7becd8138ba6e7f0
MoonMoon2 <tate@tatesworld.com>
2019-02-21 17:11:30 +0100

    More pages

---
+++ js/copytoclip.js
@@ -0,0 +1,26 @@
+function copyToClipboard(str) {
+    // Create new element
+    var el = document.createElement('textarea');
+    // Set value (string to be copied)
+    el.value = str;
+    // Set non-editable to avoid focus and move outside of view
+    el.setAttribute('readonly', '');
+    el.style = {
+        position: 'absolute',
+        left: '-9999px'
+    };
+    document.body.appendChild(el);
+    // Select text inside element
+    el.select();
+    // Copy text to clipboard
+    document.execCommand('copy');
+    // Remove temporary element
+    document.body.removeChild(el);
+    // Create copied message and define the banner
+    var ban = 'Copied ' + str + ' to clipboard'
+        banner = document.getElementById('banner')
+
+    banner.value = ban
+    banner.classname = 'actban'
+
+}
