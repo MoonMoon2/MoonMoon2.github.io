d3b6b82afc74e359a668904ed2fe1d7074d6b8aa
MoonMoon2 <tate@tatesworld.com>
2019-07-26 18:41:34 -0600

    Yeet

--- js/waterpoloschedule.js
+++ js/waterpoloschedule.js
@@ -1,3 +1,8 @@
+function onLoad() {
+    openTab('event', 'mens')
+}
+
+
 function openTab(evt, tabName) {
   // Declare all variables
   var i;
@@ -6,7 +11,7 @@ function openTab(evt, tabName) {
 
   // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 1; i < tabcontent.length; i++) {
+  for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
