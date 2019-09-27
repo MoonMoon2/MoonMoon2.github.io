f6e5b18d850af0d7c4de0e019b4eb459ed57b04e
MoonMoon2 <tate@tatesworld.com>
2019-09-27 12:17:03 +0200

    yeet

--- js/password.js
+++ js/password.js
@@ -1,15 +1,15 @@
 var ascii_range = (function() {
   var data = [];
-  while (data.length < 128) data.push(String.fromCharCode(data.length));
+  while (data.length < 127) data.push(String.fromCharCode(data.length));
   return function (start,stop) {
     start = start.charCodeAt(0);
     stop = stop.charCodeAt(0);
-    return (start < 0 || start > 127 || stop < 0 || stop > 127) ? null : data.slice(start,stop+1);
+    return (start < 0 || start > 126 || stop < 0 || stop > 126) ? null : data.slice(start,stop+1);
   };
 })();
-console.log(String.fromCharCode(127))
-ascii_range('A','Z').join();
-
+console.log(String.fromCharCode(126))
+ascii_range('A','~').join();
+console.log(ascii_range)
 
 function password(text,key) {
 
