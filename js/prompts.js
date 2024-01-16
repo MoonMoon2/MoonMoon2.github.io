b0d416ecc578b72e0369968af03bf59628e1b71e
Tobias Ward <tate@tatesworld.com>
2024-01-15 21:05:56 -0700

    Do it do?

--- js/prompts.js
+++ js/prompts.js
@@ -1,6 +1,9 @@
-$("#newPromptBtn").on("click", function() {
-    $("#prompt").html(promptList[Math.floor(Math.random() * promptList.length)])
-});
+document.getElementById("newPromptBtn").onclick = loadPrompt();
+
+
+function loadPrompt() {
+    document.getElementById("prompt").innerHTML = promptList[Math.floor(Math.random * promptList.length)]
+}
 
 const promptList = [
     "This is a test prompt",
